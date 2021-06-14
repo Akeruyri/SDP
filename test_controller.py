15f0ea65583e0e19e973dccffbf3bb7a4b11ada5
Louis P <38706718+Akeruyri@users.noreply.github.com>
2021-06-13 19:11:38 -0700

    loadshape to tap

--- test_controller.py
+++ test_controller.py
@@ -1,9 +1,6 @@
 
-import opendssdirect as dss
+import reg_controller
 from opendssdirect.utils import run_command
-import numpy as np
-import math
 
-# Setup Functions
-def load_circuit_model(self, path):
-    dss.run_command('compile ' + path)  # This will be done in the environment, this here just so the controller loads the model for now.
+#Load in the model
+dss.run_command(r'compile "C:\Users\louis\Desktop\SeniorDesignProject\OpenDSS\13Bus\MasterIEEE13.dss"')
