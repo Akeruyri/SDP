5c7f5c6337449d06ced3ac47a4e41f1e7de3f0e3
Louis P <38706718+Akeruyri@users.noreply.github.com>
2021-06-13 20:27:31 -0700

    loadshape to tap

--- test_controller.py
+++ test_controller.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 #Load in the model
-dss.run_command(r'compile "C:\Users\louis\Desktop\SeniorDesignProject\OpenDSS\13Bus\MasterIEEE13.dss"')
+dss.run_command(r'compile "C:\Users\louis\Desktop\SeniorDesignProject\OpenDSS\123BusSW\IEEE123MasterSW.dss"')
 dss.run_command('')
 dss.run_command('batchedit load..* Vmin=0.8')
 dss.run_command('batchedit load..* daily=PQmult')
@@ -20,13 +20,17 @@ for name in regulator_list:
 
 #Run Daily Load Flow
 for hrs in range(regulators[0].total_timesteps):
-    for reg in regulators:
+    for reg in range(len(regulators)):
         regulators[reg].switch_taps(hrs) #Change the taps
     dss.run_command('solve')
-    for reg in regulators:
+    for reg in range(len(regulators)):
         regulators[reg].get_losses(hrs) # Get the Loss
 
 #Print Loss
-print('System Losses/n' + regulators[0].system_losses)
-print('Line Losses/n' + regulators[0].line_losses)
-print('Transformer Losses/n' + regulators[0].transformer_losses)
+print('System Losses/n')
+print(regulators[0].system_losses)
+print('Line Losses/n')
+print(regulators[0].line_losses)
+print('Transformer Losses/n')
+print(regulators[0].transformer_losses)
+
