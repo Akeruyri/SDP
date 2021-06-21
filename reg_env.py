c869ecac20b9187e90b4876aea46b5e9380a9250
Louis P <38706718+Akeruyri@users.noreply.github.com>
2021-06-20 17:07:43 -0700

    Renamed environment.py

--- reg_env.py
+++ reg_env.py
@@ -1,1 +1,20 @@
-#
+import opendssdirect as dss
+from opendssdirect.utils import run_command
+import numpy as np
+import gym
+from gym import spaces
+
+class reg_env (gym.Env):
+
+    def __init__(self, path):
+        #
+    def step( self, action):
+
+    def reset(self):
+        observation = self.step(0)
+        return observation
+    def close(self):
+        dss.Basic.ClearAll()
+        return
+
+    def losses(self):
