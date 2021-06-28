fb7f8a7c6f350afb547059481d414649b8abb1a3
Akeruyri <louispelletier778@gmail.com>
2021-06-28 16:35:44 -0700

    Added Step, Reward functions.
    Reorganized functions

--- reg_env.py
+++ reg_env.py
@@ -1,4 +1,3 @@
-from reg_controller import reg_controller
 import opendssdirect as dss
 import numpy as np
 import math
@@ -10,43 +9,65 @@ class reg_env (gym.Env):
     def __init__(self, path):
         dss.Basic.ClearAll()
         #DSS Simulation Variables and Setup
-        dss.run_command('Compile "' + path + '"')
-        dss.run_command("set mode=daily stepsize=1h number=1")
-        dss.run_command("set hour = 0")
+        dss.Text.Command('Compile "' + path + '"')
+        dss.Text.Command("set mode=daily stepsize=1h number=1")
+        dss.Text.Command("set hour = 0")
 
-        #Import Regulators and generate action list
+        #Import Regulators, Generate Action and State Lists
         self.regulator_list = dss.RegControls.AllNames()
-        self.reg_ctrlrs = []
-        for name in self.regulator_list:
-            self.reg_ctrlrs.append(reg_controller(name))
-        self.action_list = (len(self.regulator_list) * 33) #33 (+-16 and 0) total actions
-
-        #Index List for actions 0 -> No Action, 1-16 -> Switch Tap to 1-16 (mapped directly), 17-33 -> Switch to Negative Taps 1-16
+        self.action_list = (len(self.regulator_list) * 33) #33 actions for each regulator * num of regulators (+-16 and 0) 
+        
+        #INCOMPLETE NEEDS WORK - THIS IS WHAT SHOULD STORE OUR REG_CONTROLLER OBJECTS
+        self.state_list = 0
 
         #DQN Parameters
         self.bufferSize = 2048
         self.done = False
+        self.action_space = spaces.Discrete(self.action_list) #Action spaced defined as a discrete list of each tap change actions, rather than multiple actions per step for simplicity
+        self.observation_space = spaces.Box() # INCOMPLETE NEEDS DISCUSSION
 
-    def step( self, action):
-        return
+    def step(self, action):
+        
+        #Get regulator to apply action too, attempt tap change
+        dss.RegControls.Name(self.reg_from_action(action))  # Set active SVR
+        self.switch_taps(action) # Attempt tap change
+
+        #Solve
+        dss.Text.Command("Solve")
+
+        return temp_observation, reward, done, info
 
     def reset(self):
         observation = self.step(0)
+        self.done = False
+        dss.Text.Command()
         return observation
 
     def close(self):
         dss.Basic.ClearAll()
         return
 
-    def losses(self):
-        lineloss = dss.Circuit.LineLosses()
-        return lineloss
-
+    #Class Specific Functions
     def reg_from_action(self, act_num):
         reg = math.floor(act_num/33)
-        return self.regulator_list[reg]
+        return self.regulator_list[reg] #Returns name of regulator
 
-    def tap_from_action(self, act_num):
+    #Other Functions
+    def Reward(losses): #The less system loss, the higher the reward. This may need to be a stored sum over the course of an episode (multiple steps)
+        return 1/losses 
+   
+    def switch_taps(action_num):
+        tap_num = tap_from_action(action_num)
+        if tap_num == 0:
+            return
+        else:
+            dss.RegControls.TapNumber(tap_num)  # Attempt a tap change on Active Regulator
+        return
+    
+    def losses():
+        return sum(dss.Circuit.LineLosses()) #All System Line Losses, used for reward. 
+
+    def tap_from_action(act_num):
         if act_num % 33 == 0: #If Action is "No Action"
             return 0
         elif (act_num % 33) > 0 and (act_num % 33) <= 16: #If Action is "1 to 16"
